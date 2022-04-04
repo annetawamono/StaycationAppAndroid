00de889b7546c1f0a011293a7a61e0d69e472b89
Fernanda Oliveira <f.meneses.oliveira@gmail.com>
2022-04-04 12:19:46 -0400

    First changes. Layout pages.

--- app/src/main/java/com/example/staycationappandroid/MainActivity.java
+++ app/src/main/java/com/example/staycationappandroid/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.staycationappandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_nextActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Fernanda
+
+        btn_nextActivity = findViewById(R.id.btn_nextActivity);
+
+        btn_nextActivity.setOnClickListener( view -> {
+            //go to list page with the dates
+
+            Intent intentDate = new Intent(MainActivity.this, listPage.class);
+            intentDate.putExtra("dateCheckIn", "2022-01-01");
+            intentDate.putExtra("dateCheckOut", "2022-01-10");
+            startActivity(intentDate);
+        });
     }
 }
