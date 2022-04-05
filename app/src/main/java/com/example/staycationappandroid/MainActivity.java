9646460bc5550944869d76e7a61b9f5bb79098ca
Anneta Wamono <annetawamono@yahoo.com.au>
2022-04-05 13:25:23 -0400

    Feature: Date range picker in MainActivity.java

--- app/src/main/java/com/example/staycationappandroid/MainActivity.java
+++ app/src/main/java/com/example/staycationappandroid/MainActivity.java
@@ -3,13 +3,42 @@ package com.example.staycationappandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnDates;
+    private TextView idSelectedDates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Fernanda
+
+        btnDates = findViewById(R.id.btnDates);
+        idSelectedDates = findViewById(R.id.idSelectedDates);
+
+        // Material date picker
+        MaterialDatePicker.Builder builder = MaterialDatePicker.Builder.dateRangePicker();
+
+        builder.setTitleText("Select your Check in and Check out date");
+
+        MaterialDatePicker materialDatePicker = builder.build();
+
+        btnDates.setOnClickListener(view -> {
+            materialDatePicker.show(getSupportFragmentManager(), "DATE_PICKER");
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                idSelectedDates.setText(materialDatePicker.getHeaderText());
+            }
+        });
+
     }
 }
