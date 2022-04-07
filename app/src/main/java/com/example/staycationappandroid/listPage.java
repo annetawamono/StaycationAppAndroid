0f7dc76fcd03255faa2425c7fa3ae651b4a832d7
Fernanda Oliveira <f.meneses.oliveira@gmail.com>
2022-04-07 12:50:01 -0400

    Changes Apr7. - activity_main.xml (new button to go next activity_list_page.xml) - MainActivity.java (new button to go next activity_list_page.xml) - build.gradle (insert the database and Glide libraries) My changes - activity_list_page.xml - list_page_layout.xml - listPage.java

--- app/src/main/java/com/example/staycationappandroid/listPage.java
+++ app/src/main/java/com/example/staycationappandroid/listPage.java
@@ -9,10 +9,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class listPage extends AppCompatActivity {
 
+    private Query query;
     private AccommodationAdapter adapter;
     private RecyclerView rView;
     private TextView txtTitlePage;
@@ -22,29 +25,43 @@ public class listPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_page);
 
-        getSupportActionBar().setTitle("Staycation App");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Utilities lUtilities = new Utilities();
 
-        Intent i = getIntent();
-        String checkInDate =  i.getStringExtra("checkInDate");
-        String checkOutDate =  i.getStringExtra("checkOutDate");
+        //getSupportActionBar().setTitle("Staycation App");
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Intent i = getIntent();
+        //String checkInDate =  i.getStringExtra("checkInDate");
+        //String checkOutDate =  i.getStringExtra("checkOutDate");
+        String checkInDate =  "2022-04-15";
+        String checkOutDate = "2022-04-20";
+
         txtTitlePage = findViewById(R.id.txtTitlePage);
 
         //Showing the period selected before
-        txtTitlePage.setText(checkInDate + " to " + checkOutDate);
+        txtTitlePage.setText("Period: "+ lUtilities.formatDate(checkInDate) + " to " + lUtilities.formatDate(checkOutDate));
 
         //getting the information from DB
-        query = FirebaseDatabase.getInstance().getReference().child(category);
-        FirebaseRecyclerOptions<Product> options = new FirebaseRecyclerOptions.Builder<Product>()
-                .setQuery(query, Product.class)
+        //query = FirebaseDatabase.getInstance().getReference().child("accommodations");
+        query = FirebaseDatabase.getInstance().getReference().child("accommodations")
+                .orderByChild("availableStart")
+                .startAt(checkInDate);
+
+        FirebaseRecyclerOptions<Accommodation> options = new FirebaseRecyclerOptions.Builder<Accommodation>()
+                .setQuery(query, Accommodation.class)
                 .build();
 
-        adapter = new ProductAdapter(options);
-
+        adapter = new AccommodationAdapter(options);
 
         rView = findViewById(R.id.rView);
         rView.setLayoutManager(new GridLayoutManager(this, 1));
         rView.setAdapter(adapter);
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 }
